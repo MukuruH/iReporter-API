fcdde89b49a4fde74948d3b7ff9635c1b46d8436
MukuruH <your email address>
2018-12-29 13:07:32 +0300

    added new tests

--- tests/test_api.py
+++ tests/test_api.py
@@ -113,6 +113,44 @@ class TestForIncident(unittest.TestCase):
         data =  json.loads(response.data)
         self.assertEqual(data["status"],404)
 
+    def test_patch_location(self):
+        
+        random_data = {
+                        "createdBy":"nino",
+                        "types":"red-flag",
+                        "location":"malaba",
+                        "status":"rejected",
+                        "Images":"ayo",
+                        "Videos":"alloha",
+                        "comment":"on the low"
+                    }
+        self.app_tester.post('/api/v1/red-flags',json =random_data)
+
+        location = {"location": "mutungo"}
+        response = self.app_tester.patch("/api/v1/red-flags/1/location" ,json=location)
+        data = json.loads(response.data)
+        self.assertEqual(data["status"],200)
+        self.assertIn(data["data"][0]["message"], "Updated red-flag record's location")
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(data["data"][0]["id"], 1)
+
+    def test_patch_location_in_index_that_is_not_existing(self):
+        
+        random_data = {
+                        "createdBy":"nino",
+                        "types":"red-flag",
+                        "location":"malaba",
+                        "status":"rejected",
+                        "Images":"ayo",
+                        "Videos":"alloha",
+                        "comment":"on the low"
+                    }
+        self.app_tester.post('/api/v1/red-flags',json =random_data)
+
+        location = {"location": "mutungo"}
+        response = self.app_tester.patch("/api/v1/red-flags/100/location" ,json=location)
+        self.assertEqual(response.status_code, 404)
+    
 
     def test_post_then_delete(self):
         
@@ -142,13 +180,18 @@ class TestForIncident(unittest.TestCase):
         self.assertEqual(data["status"],200)
        
 
-    def test_post_nothing_then_delete(self):
-            
+    def test_delete_that_doesnot_exist(self):
+    
    
-        response = self.app_tester.delete('/api/v1/red-flags/5')
-        data =  json.loads(response.data)
-        self.assertEqual(data["status"],404)
+        response = self.app_tester.delete('/api/v1/red-flags/100')
+        self.assertEqual(response.status_code, 404)
        
+
+    
+   
+    
+      
+    
 
     
       
